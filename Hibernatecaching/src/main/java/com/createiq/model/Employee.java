24ebbc1ac3c4eb0d13cc0580e13009043ce97ad0
murali <murali.vadlamudi46@gmail.com>
2022-04-09 19:32:11 +0530

    adding caching class files

---
+++ Hibernatecaching/src/main/java/com/createiq/model/Employee.java
@@ -0,0 +1,56 @@
+package com.createiq.model;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity  
+@Table(name="emp1012")  
+@Cacheable  
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)  
+public class Employee {
+	 @Id  
+	 private int id;    
+	 private String name;    
+	 private float salary;
+	  
+	public Employee() {
+		
+	}
+
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	 
+}
